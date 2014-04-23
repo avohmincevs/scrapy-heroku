04d59e9e504d5eb853aea0d3b1776b1b202f69f4
jugg <artjoms.vohmincevs@gmail.com>
2014-04-23 21:26:36 +0100

    modified utils to only include projects explicitly defined in scrapyd

--- scrapy_heroku/utils.py
+++ scrapy_heroku/utils.py
@@ -1,7 +1,19 @@
-from scrapyd.utils import get_project_list
-
+import os
 from spiderqueue import Psycopg2SpiderQueue
 
+
+def get_project_list(config):
+    """Get list of projects by inspecting the eggs dir
+        scrapyd.conf is skipped. Only explicitly defined projects
+    """
+    
+    eggs_dir = config.get('eggs_dir', 'eggs')
+    if os.path.exists(eggs_dir):
+        projects = os.listdir(eggs_dir)
+    else:
+        projects = []
+
+    return projects
 
 def get_spider_queues(config):
     queues = {}
